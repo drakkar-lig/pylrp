03fdf09377c3a6d776ba911c7ec946a0579e2764
Henry-Joseph Audéoud <henry-joseph.audeoud@univ-grenoble-alpes.fr>
2017-08-04 21:05:14 +0200

    Improve unloadable subcommand
    When a subcommand cannot be imported due to a ImportError, it is
    proposed to the client, but the help message show that it is
    unavailable, and using it raise effectively the ImportError.

--- src/lrp/__main__.py
+++ src/lrp/__main__.py
@@ -14,19 +14,29 @@ def cli(verbose):
         raise Exception("Use at most %d --verbose flags" % (len(log_levels) - 1))
 
 
+def _unavailable_subcommand(import_exception):
+    def unavailable(**kwargs):
+        raise import_exception
+
+    unavailable.__doc__ = """This command is not available: %s""" % import_exception.args
+    unavailable = click.command()(unavailable)
+    unavailable = click.argument("", nargs=1)(unavailable)
+    return unavailable
+
+
 if __name__ == "__main__":
     try:
         from lrp.linux_wrapper import daemon
 
         cli.add_command(daemon)
-    except ImportError:
-        pass
+    except ImportError as e:
+        cli.add_command(_unavailable_subcommand(e), name="daemon")
 
     try:
         from lrp.sniffer import sniff
 
         cli.add_command(sniff)
-    except ImportError:
-        pass
+    except ImportError as e:
+        cli.add_command(_unavailable_subcommand(e), name="sniff")
 
     cli()
