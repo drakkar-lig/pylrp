bd9420f1771985d540c3e6a642cfc0135e7e9d81
Henry-Joseph Audéoud <henry-joseph.audeoud@univ-grenoble-alpes.fr>
2017-08-09 10:58:09 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='pylrp',
+      version='0.0',
+      description="Implementation of the LRP routing protocol",
+      author="Henry-Joseph Audéoud",
+      author_email="henry-joseph.audeoud@univ-grenoble-alpes.fr",
+      url="https://gitlab.imag.fr/audeoudh/pylrp",
+      packages=['lrp'], package_dir={'': 'src'},
+      install_requires=['click', 'pyroute2', 'python-iptables', 'NetfilterQueue', 'scapy-python3'])
